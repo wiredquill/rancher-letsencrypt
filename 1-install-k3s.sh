c4421badbf40bf3ff9aaaa548ef2c5f9421a1f9b
wiredquill <erin@suse.com>
2025-04-20 09:41:35 -0700

    Update 1-install-k3s.sh

--- 1-install-k3s.sh
+++ 1-install-k3s.sh
@@ -7,4 +7,4 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable=traefik" INSTALL_K3S_
 # Symlink kubeconfig
 mkdir -p ~/.kube
 ln -sf /etc/rancher/k3s/k3s.yaml ~/.kube/config
-chmod 600 ~/.kube/config
+sudo chmod 600 ~/.kube/config
