af4a2eb47441484764695317502db0bbad92ebb3
wiredquill <erin@suse.com>
2025-05-14 11:48:22 -0700

    Update 2-install-cert-manager.sh

--- 2-install-cert-manager.sh
+++ 2-install-cert-manager.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
-echo "Installing cert-manager v1.17.1..."
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.17.1/cert-manager.yaml
+echo "Installing cert-manager v1.17.2..."
+
+helm repo add jetstack https://charts.jetstack.io --force-update
+helm repo update
+
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.17.2 \
+  --set crds.enabled=true
+
 kubectl rollout status deployment cert-manager -n cert-manager --timeout=120s
 kubectl rollout status deployment cert-manager-webhook -n cert-manager --timeout=120s
